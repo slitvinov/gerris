b0bfd315bf9a69170f3d72c267fcefca4a33d316
Stephane Popinet <s.popinet@niwa.co.nz>
2009-11-04 19:46:12 +0100

    Fixed compiler warnings for FES2004 Ignore-this: dbe4b6ff9c98a573535a0ae0ae320db9

--- modules/fes2004/fes2004_prediction.c
+++ modules/fes2004/fes2004_prediction.c
@@ -1,60 +1,61 @@
 #include "fes2004_lib.h"
 
 
-
-
-int fes2004_extraction (char *netcdf_filename,int nb_position,double *lat,double *lon,double **amplitude, double **phase,int nb_CPU)
+int fes2004_prediction (char *netcdf_filename,int time_reference,int nb_position,double *lat,double *lon,double *time,double *prediction,int nb_CPU)
 {
 
   /*####################################################*/
   /* variable*/
   /*####################################################*/
+
   //classical variables
   int rstatus;
-  
+    
   //prediction typedef variables
   spectrum_struct *spectrum=NULL;
-  mega_struct *P=NULL;
+   mega_struct *P=NULL;
   
  /*####################################################*/
   /* allocation*/
   /*####################################################*/
 
-  alloc_tide_spectrum(&spectrum);
-  P=calloc(nb_CPU,sizeof(mega_struct));
-  alloc_extraction_threads(P,nb_CPU);
- 
-  /*####################################################*/
-  /*init prediction spectrum*/
-  /*####################################################*/
-
-  rstatus=init_spectrum(spectrum,99);
-  if (rstatus != 0 ) print_error_2();  
-
+   alloc_tide_spectrum(&spectrum);
+   P=calloc(nb_CPU,sizeof(mega_struct));
+   alloc_prediction_threads(P,nb_CPU);
+      
+   /*####################################################*/
+   /*init prediction spectrum*/
+   /*####################################################*/
+   
+   rstatus=init_spectrum(spectrum,time_reference);
+   if (rstatus != 0 )  print_error_2();  
+   
   /*####################################################*/
   /* load data files*/
   /*####################################################*/
 
-  load_netcdf_fes2004_data(netcdf_filename,P,nb_CPU);
+   load_netcdf_fes2004_data(netcdf_filename,P,nb_CPU);
    
-
   /*####################################################*/
   /*init thread struct*/
   /*####################################################*/
 
-  init_thread_struct(nb_CPU, P, 99, lat, lon, NULL, NULL, spectrum,amplitude,phase);
+   init_thread_struct(nb_CPU, P, time_reference, lat, lon, time, prediction, spectrum,NULL,NULL);
 
   /*####################################################*/
-  /*Multithreaded extraction*/
+  /*Multithreaded prediction*/
   /*####################################################*/
-  
-   multi_t_extraction( nb_position, nb_CPU, P);
+
+   multi_t_prediction(nb_position , nb_CPU, P);
 
   /*####################################################*/
   /*free memory and exit*/
   /*####################################################*/
    free_threads(P,nb_CPU);
    free(P);
+
+
+   printf("\n------------- prediction completed -------------\n");
 
    return 0;
 }/*end*/		  
